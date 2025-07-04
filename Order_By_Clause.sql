cc533aeab00ed355fb62617452fa64d50b86be2b
Kritthik Rajeev Nair <kritthik2609@gmail.com>
2025-07-04 21:37:29 +0530

    Add files via upload

---
+++ Order_By_Clause.sql
@@ -0,0 +1,14 @@
+/*Name column in ascending order*/
+SELECT *
+FROM student
+ORDER BY Name ASC;
+
+/*Age column in descending order*/
+SELECT *
+FROM student
+ORDER BY Age DESC;
+
+/*ID column in descending order*/
+SELECT *
+FROM student
+ORDER BY ID DESC;
